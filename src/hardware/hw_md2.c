49500bd87eefecb4b2be02a398abf30d14150867
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-08-10 15:26:11 -0500

    Revert "model sprite offsets scale correctly with hires"
    This reverts commit eab4584a36d69939019eb4ee346db33ef50492fc.

--- src/hardware/hw_md2.c
+++ src/hardware/hw_md2.c
@@ -1360,8 +1360,6 @@ boolean HWR_DrawModel(gl_vissprite_t *spr)
 		spriteframe_t *sprframe;
 		INT32 mod;
 		interpmobjstate_t interp;
-		fixed_t spritexscale;
-		fixed_t spriteyscale;
 
 		if (R_UsingFrameInterpolation() && !paused)
 		{
@@ -1678,28 +1676,15 @@ boolean HWR_DrawModel(gl_vissprite_t *spr)
 		p.mirror = atransform.mirror;
 
 		if (HWR_UseShader())
-		{
 			HWD.pfnSetShader(HWR_GetShaderFromTarget(SHADER_MODEL));
-		}
-
-		// sprite scaling and offsets
 		{
 			float this_scale = FIXED_TO_FLOAT(interp.scale);
 
-			spritexscale = interp.spritexscale; spriteyscale = interp.spriteyscale;
-			float xs = this_scale * FIXED_TO_FLOAT(spritexscale);
-			float ys = this_scale * FIXED_TO_FLOAT(spriteyscale);
+			float xs = this_scale * FIXED_TO_FLOAT(interp.spritexscale);
+			float ys = this_scale * FIXED_TO_FLOAT(interp.spriteyscale);
 
-			// (this could be done better)
-			if (spr->mobj->skin && ((skin_t *)spr->mobj->skin)->flags & SF_HIRES)
-			{
-				fixed_t highresscale = ((skin_t *)spr->mobj->skin)->highresscale;
-				spritexscale = FixedMul(spritexscale, highresscale);
-				spriteyscale = FixedMul(spriteyscale, highresscale);
-			}
-
-			float ox = (this_scale * FIXED_TO_FLOAT(spritexscale)) * FIXED_TO_FLOAT(interp.spritexoffset);
-			float oy = (this_scale * FIXED_TO_FLOAT(spritexscale)) * FIXED_TO_FLOAT(interp.spriteyoffset);
+			float ox = xs * FIXED_TO_FLOAT(interp.spritexoffset);
+			float oy = ys * FIXED_TO_FLOAT(interp.spriteyoffset);
 
 			// offset perpendicular to the camera angle
 			p.x -= ox * gl_viewsin;
