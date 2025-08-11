05a5b08e2b240fb925b6f3079301e33b78eb529f
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-08-10 23:27:03 -0400

    Port "Cache start and end locations for WAD folders" from SRB2Classic
    https://git.do.srb2.org/STJr/SRB2/-/merge_requests/2682

--- src/m_aatree.c
+++ src/m_aatree.c
@@ -2,7 +2,7 @@
 //-----------------------------------------------------------------------------
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2000 by DooM Legacy Team.
-// Copyright (C) 1999-2023 by Sonic Team Junior.
+// Copyright (C) 1999-2025 by Sonic Team Junior.
 //
 // This program is free software distributed under the
 // terms of the GNU General Public License, version 2.
@@ -20,7 +20,7 @@
 
 typedef struct aatree_node_s
 {
-	INT32	key;
+	void*	key;
 	void*	value;
 
 	struct aatree_node_s *left, *right;
@@ -42,8 +42,10 @@ aatree_t *M_AATreeAlloc(UINT32 flags)
 
 static void M_AATreeFree_Node(aatree_node_t *node)
 {
-	if (node->left) M_AATreeFree_Node(node->left);
-	if (node->right) M_AATreeFree_Node(node->right);
+	if (node->left)
+		M_AATreeFree_Node(node->left);
+	if (node->right)
+		M_AATreeFree_Node(node->right);
 	Z_Free(node);
 }
 
@@ -103,67 +105,84 @@ static aatree_node_t *M_AATreeRebalance(aatree_node_t *node)
 	return node;
 }
 
-static aatree_node_t *M_AATreeSet_Node(aatree_node_t *node, UINT32 flags, INT32 key, void* value)
+static aatree_node_t *M_AATreeSet_Node(aatree_node_t *node, UINT32 flags, void* key, void* value, aatree_comp_t callback, aatree_dealloc_t deallocator)
 {
 	if (!node)
 	{
 		// Nothing here, so just add where we are
 		node = Z_Malloc(sizeof (aatree_node_t), PU_STATIC, NULL);
 		node->key = key;
-		if (value && (flags & AATREE_ZUSER)) Z_SetUser(value, &node->value);
-		else node->value = value;
+		if (value && (flags & AATREE_ZUSER))
+			Z_SetUser(value, &node->value);
+		else
+			node->value = value;
 		node->left = node->right = NULL;
 	}
 	else
 	{
-		if (key < node->key)
-			node->left = M_AATreeSet_Node(node->left, flags, key, value);
-		else if (key > node->key)
-			node->right = M_AATreeSet_Node(node->right, flags, key, value);
+		if (callback(key, node->key) < 0)
+			node->left = M_AATreeSet_Node(node->left, flags, key, value, callback, deallocator);
+		else if (callback(key, node->key) > 0)
+			node->right = M_AATreeSet_Node(node->right, flags, key, value, callback, deallocator);
 		else
 		{
-			if (value && (flags & AATREE_ZUSER)) Z_SetUser(value, &node->value);
-			else node->value = value;
+			if (value && (flags & AATREE_ZUSER))
+				Z_SetUser(value, &node->value);
+			else
+				node->value = value;
+
+			if (deallocator)
+				deallocator(key);
 		}
 
 		node = M_AATreeRebalance(node);
 	}
-
+	
 	return node;
 }
 
-void M_AATreeSet(aatree_t *aatree, INT32 key, void* value)
+void M_AATreeSet(aatree_t *aatree, void* key, void* value, aatree_comp_t callback, aatree_dealloc_t deallocator)
 {
-	aatree->root = M_AATreeSet_Node(aatree->root, aatree->flags, key, value);
+	I_Assert(callback != NULL);
+	aatree->root = M_AATreeSet_Node(aatree->root, aatree->flags, key, value, callback, deallocator);
 }
 
 // Caveat: we don't distinguish between nodes that don't exists
 // and nodes with value == NULL.
-static void *M_AATreeGet_Node(aatree_node_t *node, INT32 key)
+static void *M_AATreeGet_Node(aatree_node_t *node, void* key, aatree_comp_t callback, aatree_dealloc_t deallocator)
 {
 	if (node)
 	{
-		if (node->key == key)
+		if (callback(key, node->key) == 0)
+		{
+			if (deallocator)
+				deallocator(key);
 			return node->value;
-		else if(node->key < key)
-			return M_AATreeGet_Node(node->right, key);
+		}
+		else if(callback(node->key, key) < 0)
+			return M_AATreeGet_Node(node->right, key, callback, deallocator);
 		else
-			return M_AATreeGet_Node(node->left, key);
+			return M_AATreeGet_Node(node->left, key, callback, deallocator);
 	}
 
+	if (deallocator)
+		deallocator(key);
 	return NULL;
 }
 
-void *M_AATreeGet(aatree_t *aatree, INT32 key)
+void *M_AATreeGet(aatree_t *aatree, void* key, aatree_comp_t callback, aatree_dealloc_t deallocator)
 {
-	return M_AATreeGet_Node(aatree->root, key);
+	I_Assert(callback != NULL);
+	return M_AATreeGet_Node(aatree->root, key, callback, deallocator);
 }
 
 static void M_AATreeIterate_Node(aatree_node_t *node, aatree_iter_t callback)
 {
-	if (node->left) M_AATreeIterate_Node(node->left, callback);
+	if (node->left)
+		M_AATreeIterate_Node(node->left, callback);
 	callback(node->key, node->value);
-	if (node->right) M_AATreeIterate_Node(node->right, callback);
+	if (node->right)
+		M_AATreeIterate_Node(node->right, callback);
 }
 
 void M_AATreeIterate(aatree_t *aatree, aatree_iter_t callback)
